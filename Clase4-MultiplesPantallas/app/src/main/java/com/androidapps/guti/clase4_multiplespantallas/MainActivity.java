8358cddf11101a1356773404ce367a241ceb5292
android <ramiro.a.gutierrez@gmail.com>
2017-09-19 21:36:27 -0300

    Se envia información de una activity a otra

--- Clase4-MultiplesPantallas/app/src/main/java/com/androidapps/guti/clase4_multiplespantallas/MainActivity.java
+++ Clase4-MultiplesPantallas/app/src/main/java/com/androidapps/guti/clase4_multiplespantallas/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,10 +22,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         Log.d("Main Activity", "Click on button");
-        Intent intent = new Intent(this, SecondaryActivity.class);
 
+        EditText txt = (EditText)findViewById(R.id.editText);
+        String textContent = txt.getText().toString();      //Valor que quiero transmitir a la otra Activity
+
+        Intent intent = new Intent(this, SecondaryActivity.class);
         //new Intent(objeto context => se pasa directamente la vista actual,
         //objeto tipo class => acá va la clase de la activity que quiero invocar
+
+        intent.putExtra("textToShow", textContent);
 
         startActivity(intent);
     }
--- Clase4-MultiplesPantallas/app/src/main/java/com/androidapps/guti/clase4_multiplespantallas/SecondaryActivity.java
+++ Clase4-MultiplesPantallas/app/src/main/java/com/androidapps/guti/clase4_multiplespantallas/SecondaryActivity.java
@@ -1,7 +1,9 @@
 package com.androidapps.guti.clase4_multiplespantallas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondaryActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SecondaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+
+        Intent intent = getIntent(); //obtiene el intent que invocó la vista
+        Bundle bundle = intent.getExtras();     //Objeto bundle contendrá la tabla clave-valor que guarda el intent
+
+        TextView txt = (TextView)findViewById(R.id.textView);
+        txt.setText(bundle.getString("textToShow"));
+
     }
+
+
 }
